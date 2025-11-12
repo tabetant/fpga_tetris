680c9b554551177d1335aa638122b88416b58eb7
VJunez <verdy.junez@gmail.com>
2025-11-12 15:26:58 -0500

    Update piece_offsets.v

--- piece_offsets.v
+++ piece_offsets.v
@@ -36,20 +36,151 @@ module tetris_piece_offsets (
             dx3 = 2'd3; dy3 = 2'd0;
         end
 
-            // L
-        else if (shape_id == 3'd1 && (rot ==2'd0 || rot == 2'd2)) begin // rotating 0 and 2 times will result in same pos
+            // J
+        else if (shape_id == 3'd2 && (rot ==2'd0)) begin // rotating 0
             dx0 = 2'd0; dy0 = 2'd0;
             dx1 = 2'd0; dy1 = 2'd1;
-            dx2 = 2'd0; dy2 = 2'd2;
-            dx3 = 2'd0; dy3 = 2'd3;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd2; dy3 = 2'd1;
         end
 
-        else if (shape_id == 1'b1 && (rot ==2'd1 || rot == 2'd3)) begin // rotating 1 and 3 times will result in same pos
+        else if (shape_id == 3'd2 && (rot ==2'd1)) begin // rotating 1
+            dx0 = 2'd2; dy0 = 2'd0;
+            dx1 = 2'd1; dy1 = 2'd0;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd2;
+        end
+
+        else if (shape_id == 3'd2 && (rot ==2'd2)) begin // rotating 2
+            dx0 = 2'd0; dy0 = 2'd1;
+            dx1 = 2'd1; dy1 = 2'd1;
+            dx2 = 2'd2; dy2 = 2'd1;
+            dx3 = 2'd2; dy3 = 2'd2;
+        end
+
+        else if (shape_id == 3'd2 && (rot ==2'd3)) begin // rotating 3
+            dx0 = 2'd0; dy0 = 2'd2;
+            dx1 = 2'd1; dy1 = 2'd0;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd2;
+        end
+
+            // L
+        else if (shape_id == 3'd3 && (rot ==2'd0)) begin // rotating 0
+            dx0 = 2'd2; dy0 = 2'd0;
+            dx1 = 2'd0; dy1 = 2'd1;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd2; dy3 = 2'd1;
+        end
+
+        else if (shape_id == 3'd3 && (rot ==2'd1)) begin // rotating 1
+            dx0 = 2'd2; dy0 = 2'd2;
+            dx1 = 2'd1; dy1 = 2'd0;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd2;
+        end
+
+        else if (shape_id == 3'd3 && (rot ==2'd2)) begin // rotating 2
+            dx0 = 2'd0; dy0 = 2'd1;
+            dx1 = 2'd1; dy1 = 2'd1;
+            dx2 = 2'd2; dy2 = 2'd1;
+            dx3 = 2'd0; dy3 = 2'd2;
+        end
+
+        else if (shape_id == 3'd3 && (rot ==2'd3)) begin // rotating 3
             dx0 = 2'd0; dy0 = 2'd0;
             dx1 = 2'd1; dy1 = 2'd0;
-            dx2 = 2'd2; dy2 = 2'd0;
-            dx3 = 2'd3; dy3 = 2'd0;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd2;
         end
 
+                // S
+        else if (shape_id == 3'd4 && (rot ==2'd0)) begin // rotating 0
+            dx0 = 2'd2; dy0 = 2'd0;
+            dx1 = 2'd1; dy1 = 2'd0;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd0; dy3 = 2'd1;
+        end
+
+        else if (shape_id == 3'd4 && (rot ==2'd1)) begin // rotating 1
+            dx0 = 2'd2; dy0 = 2'd2;
+            dx1 = 2'd2; dy1 = 2'd1;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd0;
+        end
+
+        else if (shape_id == 3'd4 && (rot ==2'd2)) begin // rotating 2
+            dx0 = 2'd0; dy0 = 2'd2;
+            dx1 = 2'd1; dy1 = 2'd2;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd2; dy3 = 2'd1;
+        end
+
+        else if (shape_id == 3'd4 && (rot ==2'd3)) begin // rotating 3
+            dx0 = 2'd0; dy0 = 2'd0;
+            dx1 = 2'd0; dy1 = 2'd1;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd2;
+        end
+
+                // T
+        else if (shape_id == 3'd5 && (rot ==2'd0)) begin // rotating 0
+            dx0 = 2'd2; dy0 = 2'd1;
+            dx1 = 2'd1; dy1 = 2'd0;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd0; dy3 = 2'd1;
+        end
+
+        else if (shape_id == 3'd5 && (rot ==2'd1)) begin // rotating 1
+            dx0 = 2'd1; dy0 = 2'd2;
+            dx1 = 2'd2; dy1 = 2'd1;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd0;
+        end
+
+        else if (shape_id == 3'd5 && (rot ==2'd2)) begin // rotating 2
+            dx0 = 2'd0; dy0 = 2'd1;
+            dx1 = 2'd1; dy1 = 2'd2;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd2; dy3 = 2'd1;
+        end
+
+        else if (shape_id == 3'd5 && (rot ==2'd3)) begin // rotating 3
+            dx0 = 2'd1; dy0 = 2'd0;
+            dx1 = 2'd0; dy1 = 2'd1;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd1; dy3 = 2'd2;
+        end
+
+                // Z
+        else if (shape_id == 3'd6 && (rot ==2'd0)) begin // rotating 0
+            dx0 = 2'd2; dy0 = 2'd1;
+            dx1 = 2'd1; dy1 = 2'd0;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd0; dy3 = 2'd0;
+        end
+
+        else if (shape_id == 3'd6 && (rot ==2'd1)) begin // rotating 1
+            dx0 = 2'd1; dy0 = 2'd2;
+            dx1 = 2'd2; dy1 = 2'd1;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd2; dy3 = 2'd0;
+        end
+
+        else if (shape_id == 3'd6 && (rot ==2'd2)) begin // rotating 2
+            dx0 = 2'd0; dy0 = 2'd1;
+            dx1 = 2'd1; dy1 = 2'd2;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd2; dy3 = 2'd2;
+        end
+
+        else if (shape_id == 3'd6 && (rot ==2'd3)) begin // rotating 3
+            dx0 = 2'd1; dy0 = 2'd0;
+            dx1 = 2'd0; dy1 = 2'd1;
+            dx2 = 2'd1; dy2 = 2'd1;
+            dx3 = 2'd0; dy3 = 2'd2;
+        end
+        
+        
     end
 endmodule
